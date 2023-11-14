275d84bd654461a57b3f94a56ff0057e54dcdda1
Bebe1s <127968966+Bebe1s@users.noreply.github.com>
2023-11-14 09:26:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+let firstNumber = game.askForNumber("") + game.askForNumber("")
+let secondNumber = firstNumber - game.askForNumber("")
+let thirdnumber = game.askForNumber("") * secondNumber
+let fourth_number = game.askForNumber("") / thirdnumber
+game.splash(fourth_number)
